49b03f94eeb6853df2cb6f499132a921933b921f
Subhadeep Adhikary <subhadeepadhikary049@gmail.com>
2026-05-25 11:24:00 +0000

    data type file compleated

--- 01_basic/datatype.js
+++ 01_basic/datatype.js
@@ -6,3 +6,22 @@
 console.log(3+3)
 
 console.log("JavaScript")
+
+let name="javaScipt"
+let age=5
+let isLoggedIn=true
+let state= null
+let seam;
+
+//number -> 2 to power 53
+//bigInt 
+//String ->""
+//boolean -> true/false
+//null ->Standalone value
+//undefined ->
+//symbol ->unique
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);

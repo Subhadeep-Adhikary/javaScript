e8b586e3e919f966ad80f044cd722dca5f31deb4
Subhadeep Adhikary <subhadeepadhikary049@gmail.com>
2026-05-25 11:42:38 +0000

    conversions and oparations

---
+++ 01_basic/conversion_and_oparations.js
@@ -0,0 +1,30 @@
+let score= false
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber= Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+//"33" => 33
+//"33abc" => NaN
+//null => 0
+//true => 1
+
+let isLoggedIn=undefined
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 => true; 0 => false;
+//"" => false; "Something"=> true 
+//null/undefined => false
+
+let someNumber=33
+
+let StringNumber= String(someNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)

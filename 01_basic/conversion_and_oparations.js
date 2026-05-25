90cb54f2aaf33866589ed58e5c0ffd1099506fd1
Subhadeep Adhikary <subhadeepadhikary049@gmail.com>
2026-05-25 12:06:02 +0000

    oparstions

--- 01_basic/conversion_and_oparations.js
+++ 01_basic/conversion_and_oparations.js
@@ -28,3 +28,37 @@ let someNumber=33
 let StringNumber= String(someNumber)
 console.log(StringNumber)
 console.log(typeof StringNumber)
+
+
+
+
+
+//---------------------Oparations---------------------
+
+let value =3
+let negvalue= -value
+console.log(negvalue)
+
+let str1="Hello";
+let str2="World"
+
+let fullstr= str1 + " " + str2
+console.log(fullstr)
+
+console.log("1"+ 2)
+console.log(typeof("1"+2));
+console.log(1+'2')
+console.log("1"+2+2)
+console.log(1+2+'2')
+console.log(+true)
+console.log(+"")
+
+let num1,num2,num3
+
+num1=num2=num3=2*3+1
+console.log(num1)
+
+let gameCounter =100
+++gameCounter;
+console.log(gameCounter);
+

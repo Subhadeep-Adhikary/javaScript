77b5144e437c522d63b0ceb3ff96f343365d4b10
Subhadeep Adhikary <subhadeepadhikary049@gmail.com>
2026-05-26 06:57:39 +0000

    Math library

--- 01_basic/numbers_and_math.js
+++ 01_basic/numbers_and_math.js
@@ -14,3 +14,19 @@ console.log(hundreads.toLocaleString());
 
 
 
+//++++++++++++++++++++++++++++Math+++++++++++++++++++++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.98))
+console.log(Math.max(4 , 3 , 9 , 2))
+console.log(Math.min(4 , 3 , 9 , 2))
+
+console.group(Math.random()*10+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)))

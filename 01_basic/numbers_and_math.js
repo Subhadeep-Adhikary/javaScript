ea414505164e686026ab3f599ccf53d69fc04036
Subhadeep Adhikary <subhadeepadhikary049@gmail.com>
2026-05-26 06:37:48 +0000

    numbers

---
+++ 01_basic/numbers_and_math.js
@@ -0,0 +1,16 @@
+const score=400
+
+const balance = new Number(100)
+console.log(balance.toString())
+console.log(balance.toString().length)
+console.log(balance.toFixed(1));
+
+const numbers = 23.8966
+
+console.log(numbers.toPrecision(3))
+
+const hundreads = 1000000
+console.log(hundreads.toLocaleString());
+
+
+

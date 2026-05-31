582b58dc79a0be3fbb54bef519a214cf9fe2a93c
Subhadeep Adhikary <subhadeepadhikary049@gmail.com>
2026-05-31 07:07:15 +0000

    scopes

---
+++ 03_basic/scopes.js
@@ -0,0 +1,53 @@
+// var c=300
+let a=300
+
+if(true){
+    let a=10
+    const b= 20
+    var c= 30
+    console.log("Inner: ",a)
+}
+
+console.log(a)
+// console.log(b)
+console.log(c)
+
+function one(){
+    const username="Subhadeep"
+
+    function two(){
+        const website="Youtube"
+        console.log(username)
+    }
+    //console.log(website);
+    two()
+}
+
+one()
+
+if(true){
+    const username="Subhadeep";
+    if(username==="Subhadeep"){
+        const website="Github"
+        console.log(username+" "+website)
+    }
+    // console.log(website)
+}
+
+// console.log(username)
+
+
+//++++++++++++++++++++++++ Interesting +++++++++++++++++++++++++++++
+
+console.log(addone(5))
+function addone(num){
+    return num+1
+}
+
+
+// console.log(addTwo(5))
+const addTwo=function(num){
+    return num+2
+}
+
+addTwo(5)

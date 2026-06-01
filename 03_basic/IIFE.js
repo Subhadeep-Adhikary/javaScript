1ffb2701a009127675d2b9cea2491abae3ab2c65
Subhadeep Adhikary <subhadeepadhikary049@gmail.com>
2026-06-01 04:21:02 +0000

    IIFE

--- 03_basic/IIFE.js
+++ 03_basic/IIFE.js
@@ -1,1 +1,11 @@
 //Immediately Invoke Function Expressions
+
+//named IIFE
+(function DB(){
+    console.log('DB Connected');
+})();
+
+//unnanmed IIFE
+( (name)=>{
+    console.log(`DB Connected Two ${name} `)
+})("SQL");

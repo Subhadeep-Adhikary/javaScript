dc9126ad93460ff0f61fe345ae57af0c30ee0f20
Subhadeep Adhikary <subhadeepadhikary049@gmail.com>
2026-06-02 06:30:31 +0000

    while loop

---
+++ 05_iterations/while.js
@@ -0,0 +1,19 @@
+let index=0
+while (index<=10) {
+    console.log(`value of index is ${index}`)
+    index= index+2
+}
+
+let myarray=["Flash","Superman","Batman"]
+let arr=0
+while(arr < myarray.length){
+    console.log(`Value is ${myarray[arr]}`)
+    arr=arr+1
+}
+
+let score=1
+
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while(score<=10)

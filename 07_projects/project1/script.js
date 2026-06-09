56988a0049f6b38ec20a68187192ebb13e64b1aa
Subhadeep Adhikary <subhadeepadhikary049@gmail.com>
2026-06-09 16:57:50 +0000

    updated script

--- 07_projects/project1/script.js
+++ 07_projects/project1/script.js
@@ -1,10 +1,7 @@
 const body=document.querySelector('body')
 const buttons=document.querySelectorAll('.button')
 buttons.forEach(function(button){
-    console.log(button);
     button.addEventListener('click',function(e){
-        console.log(e);
-        console.log(e.target)
         body.style.backgroundColor=`${button.id}`;
     })
 });

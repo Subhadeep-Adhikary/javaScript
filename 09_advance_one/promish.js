22b19be70361a63d758e87fc41111b903dffcd3d
Subhadeep Adhikary <subhadeepadhikary049@gmail.com>
2026-06-11 12:35:24 +0000

    promish and async function

--- 09_advance_one/promish.js
+++ 09_advance_one/promish.js
@@ -22,8 +22,8 @@ new Promise(function(resolve,reject){
 
 const promishThree= new Promise(function(resolve,reject){
     setTimeout(function(){
-        resolve({username:"js",email:"javascript@example.com"},1000)
-    })
+        resolve({username:"js",email:"javascript@example.com"})
+    },1000)
 })
 promishThree.then(function(user){
     console.log(user);
@@ -33,7 +33,65 @@ const promishFour=new Promise(function(resolve,reject){
     setTimeout(function(){
         let error=true
         if(!error){
-            resolve({username})
+            resolve({username:"Aman",password:"123"})
+        }else{
+            reject("Error: something went wrong")
         }
-    })
+    },1000)
 })
+
+promishFour
+.then((user)=>{
+    console.log('user');
+    return user.username;
+})
+.then((username)=>{
+  console.log(username);
+})
+.catch(function(error){
+    console.log(error)
+})
+.finally(()=>console.log("the Promish is either resolved or rejected"))
+
+console.log('username')
+
+const promishfive=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=true
+        if(!error){
+            resolve({username:"Amar",password:"153"})
+        }else{
+            reject('Error: JS went wrong')
+        }
+    },1000)
+});
+async function consumePromishFive(){
+    try{
+  const response=await promishfive
+  console.log(response);
+  }
+  catch(error){
+    console.log(error)
+  }
+}
+
+consumePromishFive()
+
+async function getALlUsers(){
+    try{
+    const response=await fetch('https://jsonplaceholder.typicode.com/users')
+    const data=await response.json()
+    console.log(data)}
+    catch(error){
+        console.log("E :",error);
+    }
+}
+getALlUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
+}).then((data)=>{
+    console.log(data);
+}
+).catch((error)=>console.log(error))
